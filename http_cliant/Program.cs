0453c2216004dcc408206f100b48d0320a8571e0
将貴 米川 <masaki.2.28.my.f@gmail.com>
2023-06-23 10:43:57 +0900

    プロジェクト ファイルを追加します。

---
+++ http_cliant/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Net;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 送信先のURLを指定
+        string url = "http://localhost:8080/";
+
+        // 送信するデータを指定
+        string requestData = "Hello, Server! This is the data I'm sending.";
+
+        // HTTPリクエストの作成
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        request.Method = "POST";
+
+        byte[] requestDataBytes = Encoding.UTF8.GetBytes(requestData);
+        request.ContentLength = requestDataBytes.Length;
+
+        using (Stream requestStream = request.GetRequestStream())
+        {
+            requestStream.Write(requestDataBytes, 0, requestDataBytes.Length);
+        }
+
+        // レスポンスの受信
+        using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+        {
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(responseStream))
+                {
+                    string responseText = reader.ReadToEnd();
+                    Console.WriteLine("サーバーからのレスポンス: " + responseText);
+                }
+            }
+        }
+    }
+}
---
+++ http_server_sample/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // リスナーの作成
+        HttpListener listener = new HttpListener();
+        listener.Prefixes.Add("http://localhost:8080/"); // 待ち受けるURLを指定
+
+        try
+        {
+            listener.Start();
+            Console.WriteLine("サーバーが起動しました。");
+
+            while (true)
+            {
+                // リクエストの受信待ち
+                HttpListenerContext context = listener.GetContext();
+
+                // リクエストのデータを保存
+                string requestData;
+                using (StreamReader reader = new StreamReader(context.Request.InputStream, context.Request.ContentEncoding))
+                {
+                    requestData = reader.ReadToEnd();
+                }
+
+                // リクエストデータを保存
+                string savePath = "C:\\data\\received_data.txt"; // データの保存先を指定
+                //File.WriteAllText(savePath, requestData);
+                //Console.WriteLine("データを保存しました: " + savePath);
+
+                // レスポンスの作成
+                string responseString = "データの受信が完了しました。";
+                byte[] responseData = Encoding.UTF8.GetBytes(responseString);
+                context.Response.ContentLength64 = responseData.Length;
+                context.Response.OutputStream.Write(responseData, 0, responseData.Length);
+                context.Response.OutputStream.Close();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("エラーが発生しました: " + ex.Message);
+        }
+        finally
+        {
+            listener.Stop();
+            listener.Close();
+        }
+    }
+}
